c9c90b530ef6887c11bc1c8c9e186dd9c8005534
Vault <128987423+Automation-Vault@users.noreply.github.com>
2023-12-24 03:22:06 -0700

    Updated: [SoD]Druid balance(no form)

--- [SoD]Druid balance(no form)/Rotation.cs
+++ [SoD]Druid balance(no form)/Rotation.cs
@@ -1,273 +1,208 @@
 using System;
-using System.Threading;
 using wShadow.Templates;
 using wShadow.Warcraft.Classes;
 using wShadow.Warcraft.Defines;
 using wShadow.Warcraft.Managers;
-using wShadow.Warcraft.Structures.Wow_Player;
-using wShadow.Warcraft.Defines.Wow_Player;
-using wShadow.Warcraft.Defines.Wow_Spell;
-
-
 
 public class Druid : Rotation
 {
-	
+
     private int debugInterval = 5; // Set the debug interval in seconds
     private DateTime lastDebugTime = DateTime.MinValue;
-  private TimeSpan starsurgeCooldown = TimeSpan.FromSeconds(6.5);
-	private DateTime laststarsurgeTime = DateTime.MinValue;
-	
+    private TimeSpan starsurgeCooldown = TimeSpan.FromSeconds(6.5);
+    private DateTime laststarsurgeTime = DateTime.MinValue;
+
     public override void Initialize()
     {
-        // Can set min/max levels required for this rotation.
-        
-		 lastDebugTime = DateTime.Now;
+        lastDebugTime = DateTime.Now;
         LogPlayerStats();
-        // Use this method to set your tick speeds.
-        // The simplest calculation for optimal ticks (to avoid key spam and false attempts)
 
-		// Assuming wShadow is an instance of some class containing UnitRatings property
         SlowTick = 600;
         FastTick = 200;
-
-        // You can also use this method to add to various action lists.
-
-        // This will add an action to the internal passive tick.
-        // bool: needTarget -> If true action will not fire if player does not have a target
-        // Func<bool>: function -> Action to attempt, must return true or false.
-        PassiveActions.Add((true, () => false));
-
-        // This will add an action to the internal combat tick.
-        // bool: needTarget -> If true action will not fire if player does not have a target
-        // Func<bool>: function -> Action to attempt, must return true or false.
-        CombatActions.Add((true, () => false));
-		
-		
-		
     }
-	public override bool PassivePulse()
-	{
-			
 
-		 var me = Api.Player;
-		var healthPercentage = me.HealthPercent;
-		var mana = me.Mana;
-
-			if (me.IsDead() || me.IsGhost() || me.IsCasting()|| me.IsMoving() ) return false;
+    public override bool PassivePulse()
+    {
+        var me = Api.Player;
+        if (!me.IsValid()) return false;
+        if (me.IsDead() || me.IsGhost() || me.IsCasting() || me.IsMoving()) return false;
         if (me.HasAura("Drink") || me.HasAura("Food")) return false;
-		if ((DateTime.Now - lastDebugTime).TotalSeconds >= debugInterval)
+
+        var mana = me.Mana;
+        var healthPercentage = me.HealthPercent;
+
+        if ((DateTime.Now - lastDebugTime).TotalSeconds >= debugInterval)
         {
             LogPlayerStats();
             lastDebugTime = DateTime.Now; // Update lastDebugTime
         }
 
-
-if (Api.Spellbook.CanCast("Mark of the Wild") && !me.HasAura("Mark of the Wild")  )
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Casting Mark of the Wild");
-    Console.ResetColor();
-    if (Api.Spellbook.Cast("Mark of the Wild"))
-   
-        return true;
-    }
-	if (Api.Spellbook.CanCast("Thorns") && !me.HasAura("Thorns")  )
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Casting Thorns");
-    Console.ResetColor();
-    if (Api.Spellbook.Cast("Thorns"))
-   
-        return true;
-    }
-
-	if (Api.Spellbook.CanCast("Omen of Clarity") && !me.HasAura("Omen of Clarity")  )
-{
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine("Casting Omen of Clarity");
-    Console.ResetColor();
-    if (Api.Spellbook.Cast("Omen of Clarity"))
-   
-        return true;
-    }
-	
-			 var target = Api.Target;
-
-			if (!target.IsDead())
-				{
-				
-if (Api.Spellbook.CanCast("Moonfire") && !target.HasAura("Moonfire") )
-{
-    var reaction = me.GetReaction(target);
-    
-    if (reaction != UnitReaction.Friendly)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Casting Moonfire");
-        Console.ResetColor();
-        
-        if (Api.Spellbook.Cast("Moonfire"))
+        if (Api.Spellbook.CanCast("Mark of the Wild") && 
+            (!me.HasAura("Mark of the Wild") || me.AuraRemains("Mark of the Wild") <= 30000))
         {
-            return true; // Successful cast of Wrath
+            Print("Casting Mark of the Wild", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Mark of the Wild"))
+                return true;
         }
-    }
-    // If unable to cast Moonfire, proceed to the next spell
-}
-else 
-if (Api.Spellbook.CanCast("Wrath") )
-{
-    var reaction = me.GetReaction(target);
-    
-    if (reaction != UnitReaction.Friendly)
-    {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine("Casting Wrath");
-        Console.ResetColor();
-        
-        if (Api.Spellbook.Cast("Wrath"))
+
+        if (Api.Spellbook.CanCast("Thorns") && !me.HasAura("Thorns"))
         {
-            return true; // Successful cast of Wrath
+            Print("Casting Thorns", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Thorns"))
+                return true;
         }
+
+        if (Api.Spellbook.CanCast("Omen of Clarity") && !me.HasAura("Omen of Clarity"))
+        {
+            Print("Casting Omen of Clarity", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Omen of Clarity"))
+                return true;
+        }
+
+        var target = Api.Target;
+        if (!target.IsValid() || target.IsDeadOrGhost()) return false;
+        if (me.GetReaction(target) >= UnitReaction.Friendly) return false;
+
+        if (Api.Spellbook.CanCast("Moonfire") && !target.HasAura("Moonfire"))
+        {
+            Print("Moonfire", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Moonfire"))
+                return true;
+            // If unable to cast Moonfire, proceed to the next spell
+        }
+        else if (Api.Spellbook.CanCast("Wrath"))
+        {
+            Print("Wrath", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Wrath"))
+                return true;
+        }
+        else
+        {
+            // Handle if unable to cast Wrath or Moonfire
+            Console.WriteLine("Unable to cast Moonfire or Wrath. Skipping cast.");
+        }
+
+        return base.PassivePulse();
     }
-    else
+
+    public override bool CombatPulse()
     {
-        // Handle if the target is friendly
-        Console.WriteLine("Target is friendly. Skipping Wrath cast.");
-    }
-}
-else
-{
-    // Handle if unable to cast Wrath or Moonfire
-    Console.WriteLine("Unable to cast Moonfire or Wrath. Skipping cast.");
-}
-
-}
-
- return base.PassivePulse();
-}				
-		
-	public override bool CombatPulse()
-    {
-				
-
         var me = Api.Player;
-		var healthPercentage = me.HealthPercent;
-		var mana = me.Mana;
-		 var target = Api.Target;
-		var targethealth = target.HealthPercent;
-		var energy = me.Energy;
-		var points = me.ComboPoints;
+        if (!me.IsValid() || me.IsDeadOrGhost()) return false;
+        if (me.IsCasting() || me.IsChanneling()) return false;
+        
+        var mana = me.Mana; 
+        var energy = me.Energy;
+        var points = me.ComboPoints;
+        var healthPercentage = me.HealthPercent;
+        
+        var target = Api.Target;
+        if (!target.IsValid() || target.IsDeadOrGhost()) return false;
+        if (me.GetReaction(target) >= UnitReaction.Friendly) return false;
 
-		if (Api.Spellbook.CanCast("Rejuvenation") &&!me.HasAura("Rejuvenation") && healthPercentage <= 70 && mana > 15)
+        var targethealth = target.HealthPercent;
+        
+        if (Api.Spellbook.CanCast("Rejuvenation") && !me.HasAura("Rejuvenation") && healthPercentage <= 70 && mana > 15)
         {
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Casting Rejuvenation");
-			Console.ResetColor();
-			if (Api.Spellbook.Cast("Rejuvenation"))
-			{
-				return true;
-			}
-		}
-		
-		if (Api.Spellbook.CanCast("Healing Touch") && healthPercentage <= 45 && mana > 25 && me.HasAura("Fury of Stormrage"))
+            Print("Casting Rejuvenation", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Rejuvenation"))
+                return true;
+        }
+
+        if (Api.Spellbook.CanCast("Healing Touch") && healthPercentage <= 45 && mana > 25 && me.HasAura("Fury of Stormrage"))
         {
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Casting Healing Touch");
-			Console.ResetColor();
-			if (Api.Spellbook.Cast("Healing Touch"))
-			{
-				return true;
-			}
-       }
-	   if (Api.Spellbook.CanCast("Healing Touch") && healthPercentage <= 45 && mana > 25 )
+            Print("Casting Healing Touch", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Healing Touch"))
+                return true;
+        }
+
+        if (Api.Spellbook.CanCast("Healing Touch") && healthPercentage <= 45 && mana > 25)
         {
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Casting Healing Touch");
-			Console.ResetColor();
-			if (Api.Spellbook.Cast("Healing Touch"))
-			{
-				return true;
-			}
-       }
-		if (!target.HasAura("Sunfire") && targethealth>30 && Api.HasMacro("Sunfire") )
+            Print("Casting Healing Touch", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Healing Touch"))
+                return true;
+        }
+
+        if ((!target.HasAura("Sunfire") || target.AuraRemains("Sunfire") <= 2500 )&& targethealth > 30 && Api.HasMacro("Sunfire"))
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Casting Sunfire");
-            Console.ResetColor();
+            Print("Casting Sunfire", ConsoleColor.Green);
             if (Api.UseMacro("Sunfire"))
             {
                 return true;
             }
         }
-		
-		if (Api.HasMacro("Starsurge"))
-        {
-            if ((DateTime.Now - laststarsurgeTime) >= starsurgeCooldown)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Casting Starsurge");
-                Console.ResetColor();
 
+        if (Api.HasMacro("Starsurge"))
+        {
+            if ((DateTime.Now - laststarsurgeTime) < starsurgeCooldown)
+                Print("Starsurge is on cooldown. Skipping cast.", ConsoleColor.DarkYellow); // If the cooldown period for Chimera Shot hasn't elapsed yet
+            else
+            {
+                Print("Casting Starsurge", ConsoleColor.Green);
                 if (Api.UseMacro("Starsurge"))
                 {
                     laststarsurgeTime = DateTime.Now;
                     return true;
                 }
             }
-            else
+        }
+
+        if (Api.Spellbook.CanCast("Moonfire") && targethealth > 30 && 
+            (!target.HasAura("Moonfire") || target.AuraRemains("Moonfire") <= 2500))
+        {
+            Print("Casting Moonfire", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Moonfire"))
             {
-                // If the cooldown period for Chimera Shot hasn't elapsed yet
-                Console.WriteLine("Starsurge is on cooldown. Skipping cast.");
+                return true;
             }
         }
-		if (Api.Spellbook.CanCast("Moonfire") && !target.HasAura("Moonfire") && targethealth>30 )
-		{
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Casting Moonfire");
-			Console.ResetColor();
-			if (Api.Spellbook.Cast("Moonfire"))
-			{
-				return true;
-			}
-		}
 
-		
+        if (Api.Spellbook.CanCast("Wrath"))
+        {
+            Print("Casting Wrath", ConsoleColor.Green);
+            if (Api.Spellbook.Cast("Wrath"))
+                return true;
+        }
 
-       if (Api.Spellbook.CanCast("Wrath") )
-	   {
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Casting Wrath");
-			Console.ResetColor();
-			if (Api.Spellbook.Cast("Wrath"))
-			{
-				return true;
-			}		   
-	   }
-
-		return base.CombatPulse();
+        return base.CombatPulse();
     }
-	
-	private void LogPlayerStats()
+
+    private void LogPlayerStats()
     {
         var me = Api.Player;
 
-		var mana = me.Mana;
+        var mana = me.Mana;
         var healthPercentage = me.HealthPercent;
-		
 
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"{mana} Mana available");
-        Console.WriteLine($"{healthPercentage}% Health available");
-		Console.ResetColor();
-Console.ResetColor();
+        Print(new []
+        {
+            $"{mana} Mana available" , 
+            $"{healthPercentage}% Health available"
+        }, ConsoleColor.Red);
 
-if (me.HasAura("Fury of Stormrage"))
-		{
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.WriteLine("Casting Fury of Stormrage");
-			Console.ResetColor();
+        if (me.HasAura("Fury of Stormrage"))
+            Print("Casting Fury of Stormrage", ConsoleColor.Green);
+
     }
-	
-}
+
+    private void Print(string message, ConsoleColor color)
+    {
+        if(string.IsNullOrEmpty(message)) return;
+        var old = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ForegroundColor = old;
+    }
+
+    private void Print(string[] messages, ConsoleColor color)
+    {
+        if(messages is {Length: <= 0}) return;
+        
+        var old = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+        for (var i = 0; i < messages.Length; i++)
+            Console.WriteLine(messages[i]);
+        Console.ForegroundColor = old;
+    }
 }
