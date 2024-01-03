717fa0c37cf1cb773fd3e4950391aadd2dde33ad
SpeedySaky <speedysaky@gmail.com>
2024-01-03 10:01:34 +0100

    Update Rotation.cs
    Added Rockbiter 1,2,3 for MH and OH

--- [SoD]EnhaShaman/Rotation.cs
+++ [SoD]EnhaShaman/Rotation.cs
@@ -12,7 +12,10 @@ using wShadow.Warcraft.Defines.Wow_Spell;
 
 public class EnhaShaman : Rotation
 {
-	
+private bool HasEnchantment(EquipmentSlot slot, string enchantmentName)
+{
+    return Api.Equipment.HasEnchantment(slot, enchantmentName);
+}
     private int debugInterval = 5; // Set the debug interval in seconds
     private DateTime lastDebugTime = DateTime.MinValue;
 	 private DateTime lastRockbiterTime = DateTime.MinValue;   
@@ -54,30 +57,46 @@ public class EnhaShaman : Rotation
 		var healthPercentage = me.HealthPercent;
 		var mana = me.Mana;
 
-		
 		if ((DateTime.Now - lastDebugTime).TotalSeconds >= debugInterval)
         {
             LogPlayerStats();
             lastDebugTime = DateTime.Now; // Update lastDebugTime
         }
 
+	bool hasRockbiterEnchantment1 = HasEnchantment(EquipmentSlot.MainHand, "Rockbiter 1");
+	bool hasRockbiterEnchantment2 = HasEnchantment(EquipmentSlot.MainHand, "Rockbiter 2");
+	bool hasRockbiterEnchantment3 = HasEnchantment(EquipmentSlot.MainHand, "Rockbiter 3");
 
+bool hasAnyRockbiterEnchantment = hasRockbiterEnchantment1 || hasRockbiterEnchantment2 || hasRockbiterEnchantment3;
 
- if ((DateTime.Now - lastRockbiterTime).TotalMinutes >= 2)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Casting Rockbiter Weapon");
-            Console.ResetColor();
+if (Api.Spellbook.CanCast("Rockbiter Weapon") && !hasAnyRockbiterEnchantment)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Casting Rockbiter Weapon");
+    Console.ResetColor();
+    if (Api.Spellbook.Cast("Rockbiter Weapon"))
+    {
+        return true;
+    }
+}
 
-            if (Api.Spellbook.CanCast("Rockbiter Weapon"))
-            {
-                if (Api.Spellbook.Cast("Rockbiter Weapon"))
-                {
-                    lastRockbiterTime = DateTime.Now; // Update lastRockbiterTime
-                    return true;
-                }
-            }
-        }
+	bool hasRockbiterEnchantment1off = HasEnchantment(EquipmentSlot.OffHand, "Rockbiter 1");
+	bool hasRockbiterEnchantment2off = HasEnchantment(EquipmentSlot.OffHand, "Rockbiter 2");
+	bool hasRockbiterEnchantment3off = HasEnchantment(EquipmentSlot.OffHand, "Rockbiter 3");
+
+bool hasAnyRockbiterEnchantment2 = hasRockbiterEnchantment1off || hasRockbiterEnchantment2off || hasRockbiterEnchantment3off;
+
+if (Api.Spellbook.CanCast("Rockbiter Weapon") && !hasAnyRockbiterEnchantment2)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Casting Rockbiter Weapon");
+    Console.ResetColor();
+    if (Api.Spellbook.Cast("Rockbiter Weapon"))
+    {
+        return true;
+    }
+}
+
 
 if (Api.Spellbook.CanCast("Ghost Wolf") && !me.HasPermanent("Ghost Wolf"))
 {
@@ -165,17 +184,38 @@ if (Api.Spellbook.CanCast("Lightning Shield") && !me.HasAura("Lightning Shield")
     }
 	
 	private void LogPlayerStats()
-    {
-        var me = Api.Player;
+{
+    var me = Api.Player;
 
-		var mana = me.Mana;
-        var healthPercentage = me.HealthPercent;
-		
+    var mana = me.Mana;
+    var healthPercentage = me.HealthPercent;
 
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"{mana} Mana available");
-        Console.WriteLine($"{healthPercentage}% Health available");
-		Console.ResetColor();
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine($"{mana} Mana available");
+    Console.WriteLine($"{healthPercentage}% Health available");
+    Console.ResetColor();
+
+bool hasRockbiterEnchantment1 = HasEnchantment(EquipmentSlot.MainHand, "Rockbiter 1");
+bool hasRockbiterEnchantment2 = HasEnchantment(EquipmentSlot.MainHand, "Rockbiter 2");
+bool hasRockbiterEnchantment3 = HasEnchantment(EquipmentSlot.MainHand, "Rockbiter 3");
+
+if (hasRockbiterEnchantment1)
+{
+    Console.WriteLine("Rockbiter 1 enchantment found on weapon");
+}
+if (hasRockbiterEnchantment2)
+{
+    Console.WriteLine("Rockbiter 2 enchantment found on weapon");
+}
+if (hasRockbiterEnchantment3)
+{
+    Console.WriteLine("Rockbiter 3 enchantment found on weapon");
+}
+
+
+
 Console.ResetColor();
-    }
+
+}
+
 }
